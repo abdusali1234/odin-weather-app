e5dc5a3d7d3c0cecb1343ffebaef39b311583d42
abdusali1234 <abdusali1234@gmail.com>
2024-09-11 13:50:20 +0100

    fixed issue with getWeatherData function returning undefined values

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { getWeatherData } from "./data";
 console.log("hello!");
 
 let { address, conditions, conditionsIcon, temp, tempFeelsLike } =
-  getWeatherData();
+  await getWeatherData();
 //      console.log(getWeatherData().address);
 
 console.log({ address, conditions, conditionsIcon, temp, tempFeelsLike });
